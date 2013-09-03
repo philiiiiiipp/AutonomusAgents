49917a545eb0fd37ed52a36587ff02d9349c6ab3
RemideZ <remi.de.z@gmail.com>
2013-09-03 11:30:50 +0200

    Made state static

--- AA/src/Predator.java
+++ AA/src/Predator.java
@@ -1,6 +1,8 @@
 public class Predator extends Agent
 {
 
+	private static final int RADIUS = 5;
+
 	public Predator(final int x, final int y)
 	{
 		super(x, y);
@@ -9,8 +11,17 @@ public class Predator extends Agent
 	@Override
 	public boolean step()
 	{
-		int rand = Agent.RAND.nextInt(5);
+		int rand = Agent.RAND.nextInt(RADIUS);
+		switch (rand)
+		{
+		case 0:
+			boolean b = State.isFree(this.coord_x - 1, this.coord_y);
+			this.coord_x -= 1;
+			return b;
+
+		default:
+			break;
+		}
 		return false;
 	}
-
 }
