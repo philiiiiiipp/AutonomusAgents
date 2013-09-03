747403a1cf1665c44721895f57200bb38d706790
anoukv <anouk.visser@student.uva.nl>
2013-09-03 11:55:12 +0200

    N, E, S, W

---
+++ AA/src/Point.java
@@ -0,0 +1,21 @@
+public class Point
+{
+	final int x;
+	final int y;
+
+	public Point(final int x, final int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX()
+	{
+		return this.x;
+	}
+
+	public int getY()
+	{
+		return this.y;
+	}
+}
--- AA/src/State.java
+++ AA/src/State.java
@@ -4,6 +4,10 @@ public class State
 {
 
 	private static ArrayList<Agent> agents;
+	private static final int XMAX = 10;
+	private static final int YMAX = 10;
+	private static final int XMIN = 0;
+	private static final int YMIN = 0;
 
 	public static boolean isFree(final int x, final int y)
 	{
@@ -17,4 +21,35 @@ public class State
 		return true;
 	}
 
+	public static Point north(final Point pointToTranslate)
+	{
+		int newY = pointToTranslate.getY() == YMIN ? YMAX : pointToTranslate
+				.getY() - 1;
+		Point p = new Point(pointToTranslate.getX(), newY);
+		return p;
+	}
+
+	public static Point south(final Point pointToTranslate)
+	{
+		int newY = pointToTranslate.getY() == YMAX ? YMIN : pointToTranslate
+				.getY() + 1;
+		Point p = new Point(pointToTranslate.getX(), newY);
+		return p;
+	}
+
+	public static Point east(final Point pointToTranslate)
+	{
+		int newX = pointToTranslate.getX() == XMAX ? XMIN : pointToTranslate
+				.getX() + 1;
+		Point p = new Point(newX, pointToTranslate.getY());
+		return p;
+	}
+
+	public static Point west(final Point pointToTranslate)
+	{
+		int newX = pointToTranslate.getX() == XMIN ? XMAX : pointToTranslate
+				.getX() + 1;
+		Point p = new Point(newX, pointToTranslate.getY());
+		return p;
+	}
 }
