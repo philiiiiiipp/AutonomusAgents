29b8713753c07e8188ba18b69f53d771aa71419f
RemideZ <remi.de.z@gmail.com>
2013-09-03 12:06:22 +0200

    Agent

--- AA/src/Agent.java
+++ AA/src/Agent.java
@@ -4,12 +4,13 @@ public abstract class Agent
 {
 
 	protected Point currentPosition;
-
+	protected final State state;
 	protected static Random RAND = new Random(0);
 
-	public Agent(final Point p)
+	public Agent(final Point p, final State s)
 	{
 		this.currentPosition = p;
+		this.state = s;
 	}
 
 	public boolean isPresent(final int x, final int y)
