9b431d1865adff76ed2c3bd674e81b4cdbb79eba
anoukv <anouk.visser@student.uva.nl>
2013-10-01 12:25:55 +0200

    hello

--- AA/src/autonomousagents/test/TestSarsa.java
+++ AA/src/autonomousagents/test/TestSarsa.java
@@ -16,7 +16,7 @@ import autonomousagents.actions.Action;
 import autonomousagents.agent.Predator;
 import autonomousagents.agent.Prey;
 import autonomousagents.policy.Policy;
-import autonomousagents.policy.predator.EGreedyPolicy;
+import autonomousagents.policy.predator.SoftmaxPolicy;
 import autonomousagents.policy.prey.PreyRandomPolicy;
 import autonomousagents.util.Constants;
 import autonomousagents.world.Environment;
@@ -25,7 +25,7 @@ import autonomousagents.world.State;
 
 public class TestSarsa
 {
-	private static final int NUMBER_OF_EPISODES = 1000;
+	private static final int NUMBER_OF_EPISODES = 10000;
 	private static final double alpha = 0.1d;
 
 	public static void test()
@@ -33,7 +33,8 @@ public class TestSarsa
 
 		double average = 0;
 		double averageLastProcent = 0;
-		Policy predatorPolicy = new EGreedyPolicy();
+		// Policy predatorPolicy = new EGreedyPolicy();
+		Policy predatorPolicy = new SoftmaxPolicy();
 		PreyRandomPolicy preyPoly = new PreyRandomPolicy();
 
 		XYSeries steps = new XYSeries("steps");
