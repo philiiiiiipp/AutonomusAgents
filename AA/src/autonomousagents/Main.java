d751209f6895c789dc852a3786980f59dd1cb1b3
Cristina <cr1st1a.garbaceagmail.com>
2013-10-03 21:27:20 +0200

    Changed function call inside main

--- AA/src/autonomousagents/Main.java
+++ AA/src/autonomousagents/Main.java
@@ -1,29 +1,20 @@
 package autonomousagents;
 
 import autonomousagents.test.TestQLearning;
-<<<<<<< HEAD
-=======
-import autonomousagents.test.TestSarsa;
-import autonomousagents.test.TestVariousQLearning;
->>>>>>> 5802596ea206390ae0bbda1a30c5972f1f6c1897
 
 public class Main
 {
 
 	public static void main(final String[] args)
 	{
-<<<<<<< HEAD
 		// TestOnPolicyMonteCarlo.test();
 
 		TestQLearning.test();
 		// TestSarsa.test();
 		// TestVariousQLearning.test();
-=======
-		TestVariousQLearning.test();
+		// TestVariousQLearning.test();
+		// TestSarsa.test();
 
-		TestQLearning.test();
-		TestSarsa.test();
->>>>>>> 5802596ea206390ae0bbda1a30c5972f1f6c1897
 		// TestValueIteration.test();
 		// TestPolicyEvaluation.test();
 		// TestPolicyIteration.test();
