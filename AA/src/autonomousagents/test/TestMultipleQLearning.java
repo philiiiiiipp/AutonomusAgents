43cada81d3a61c512cfa82eeeaebdd0b2b29c62b
RemideZ <remi.de.z@gmail.com>
2013-10-17 16:43:46 +0200

    nothing

--- AA/src/autonomousagents/test/TestMultipleQLearning.java
+++ AA/src/autonomousagents/test/TestMultipleQLearning.java
@@ -1,23 +1,16 @@
 package autonomousagents.test;
 
-import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.xy.XYSplineRenderer;
 import org.jfree.data.xy.XYSeriesCollection;
-import org.jfree.ui.ApplicationFrame;
 
 import autonomousagents.util.JFreeChartHelper;
 import autonomousagents.world.Point;
 
 public class TestMultipleQLearning
 {
-	private static final int EPISODE_COUNT = 200000;
+	private static final int EPISODE_COUNT = 1000;
 
 	/**
 	 * Plot the difference between SARSA, Q-Learning and On-/Off-Policy Monte
@@ -40,22 +33,25 @@ public class TestMultipleQLearning
 		dataset.addSeries(JFreeChartHelper.createWinningDataseries(stepList, "Q-Learning with Alpha:" + alpha
 				+ " Learning:" + EPISODE_COUNT, 1));
 
-		dataset.addSeries(JFreeChartHelper.createLoosingDataseries(stepList, "Prey" + alpha + " Gamma:" + gamma, 1));
+		System.out.println("Done");
 
-		ApplicationFrame frame = new ApplicationFrame("");
-
-		NumberAxis xax = new NumberAxis("Episodes");
-		NumberAxis yax = new NumberAxis("Steps");
-		XYSplineRenderer a = new XYSplineRenderer();
-		a.setBaseShapesVisible(false);
-		a.setSeriesPaint(3, Color.RED);
-		XYPlot xyplot = new XYPlot(dataset, xax, yax, a);
-
-		JFreeChart chart = new JFreeChart(xyplot);
-
-		ChartPanel chartPanel = new ChartPanel(chart);
-		frame.setContentPane(chartPanel);
-		frame.pack();
-		frame.setVisible(true);
+		// dataset.addSeries(JFreeChartHelper.createLoosingDataseries(stepList,
+		// "Prey" + alpha + " Gamma:" + gamma, 1));
+		//
+		// ApplicationFrame frame = new ApplicationFrame("");
+		//
+		// NumberAxis xax = new NumberAxis("Episodes");
+		// NumberAxis yax = new NumberAxis("Steps");
+		// XYSplineRenderer a = new XYSplineRenderer();
+		// a.setBaseShapesVisible(false);
+		// a.setSeriesPaint(3, Color.RED);
+		// XYPlot xyplot = new XYPlot(dataset, xax, yax, a);
+		//
+		// JFreeChart chart = new JFreeChart(xyplot);
+		//
+		// ChartPanel chartPanel = new ChartPanel(chart);
+		// frame.setContentPane(chartPanel);
+		// frame.pack();
+		// frame.setVisible(true);
 	}
 }
