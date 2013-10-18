2ad33f1d22a1fd83083c50b32775561dd2e66cd2
RemideZ <remi.de.z@gmail.com>
2013-10-18 12:56:00 +0200

    nothing

--- AA/src/autonomousagents/Main.java
+++ AA/src/autonomousagents/Main.java
@@ -1,22 +1,16 @@
 package autonomousagents;
 
-<<<<<<< HEAD
-import autonomousagents.test.a3.TestMultipleRLearning;
-=======
 import autonomousagents.test.a3.TestMiniMax;
->>>>>>> a656906c3dbd1b6a899f45b2d3eec8e1bea6f214
+import autonomousagents.test.a3.TestMultipleRLearning;
 
 public class Main
 {
 	public static void main(final String[] args)
 	{
-<<<<<<< HEAD
 		// TestNewSimulator.test();
 		// TestMultipleQLearning.test();
 		TestMultipleRLearning.test();
-=======
 		TestMiniMax.test();
->>>>>>> a656906c3dbd1b6a899f45b2d3eec8e1bea6f214
 
 	}
 }
